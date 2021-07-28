c3da56164e38531af5202a4b8c5c824755cb709d
dranelm <maciej.lenard1@gmail.com>
2021-07-28 09:24:13 +0000

    add teapot path

--- app.js
+++ app.js
@@ -6,4 +6,12 @@ app.get('/', (req, res) => {
 	res.send('Hello World! from express!');
 });
 
+app.get('/tea', (req, res) => {
+	res.set('X-tea-time', 'yes please');
+	res.status(418);
+	res.send('what about cofee');
+});
+
+
+
 app.listen(port, () => console.log(`Example ap listening on port ${port} ! ` ));
